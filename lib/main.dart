2ee64523fee29351ecdaed3cc18fcd23d0bbbad7
ThejalSanthosh <thejalsanthosh64@gmail.com>
2024-02-26 12:35:43 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
+import 'package:flutter/material.dart';
+
 void main (){
-  
+
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     
+    );
+  }
 }
